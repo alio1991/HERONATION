ef290463d0c7a1e13816083feb5bc5e059eebc94
cailhern <juablave@gmail.com>
2020-04-27 00:26:24 +0200

    Heronation 01 create login component (#8)
    * Feature: create login component
    
    * Feature: implement access view and some csss tweaks
    
    * Feature: set background according to loginType
    
    * Feature: styling access component
    
    * Feature: small changes

--- src/views/default-view/default-view.js
+++ src/views/default-view/default-view.js
@@ -40,6 +40,10 @@ export class DefaultView extends connect(store) (LitElement) {
       background-color: rgba(var(--green-color), 0.5);
     }
 
+    .selector-nothovered{
+      background-color: rgba(var(--base-color), 0.5);
+    }
+
     .selector-expanded{
       animation-duration: 1s;
       animation-name: extendDiv;
@@ -81,6 +85,16 @@ export class DefaultView extends connect(store) (LitElement) {
     // }
   }
 
+  // firstUpdated() {
+  //   if(this.shadowRoot.querySelector('.selector-citizen')){
+  //     this.shadowRoot.querySelector('.selector-citizen').addEventListener('mouseenter', this.hoverCitizen);
+  //     this.shadowRoot.querySelector('.selector-citizen').addEventListener('mouseleave', this.leaveHover);
+  //   }
+  //   if(this.shadowRoot.querySelector('.selector-corporation')){
+  //     this.shadowRoot.querySelector('.selector-corporation').addEventListener('mouseenter', this.hoverCorporation);
+  //     this.shadowRoot.querySelector('.selector-corporation').addEventListener('mouseleave', this.leaveHover);
+  //   }
+  // }
 
   render() {
     return html`
@@ -92,6 +106,16 @@ export class DefaultView extends connect(store) (LitElement) {
         </div>
     `;
   }
+
+  // hoverCitizen(){
+  //   if(this.shadowRoot.querySelector('.selector-corporation'))
+  //     this.shadowRoot.querySelector('.selector-corporation').classList.add('selector-nothovered')
+  // }
+
+  // hoverCorporation(){
+  //   if(this.shadowRoot.querySelector('.selector-citizen'))
+  //     this.shadowRoot.querySelector('.selector-citizen').classList.add('selector-nothovered')
+  // }
 
   selectUserType(userType, expanded, colapsed){
     store.dispatch(setLogin(userType));
