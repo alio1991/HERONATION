0a4c7274c2d7f3536e5a421908a38fb9aa0ec655
alio1991 <alioli_15@hotmail.com>
2020-04-27 10:52:21 +0200

    feature: user-logged component created

---
+++ src/components/user-logged/user-logged.js
@@ -0,0 +1,113 @@
+import { html, css, LitElement } from 'lit-element';
+import { connect } from 'pwa-helpers';
+import { store } from './../../redux/store.js';
+import { Router } from '@vaadin/router';
+import { setLogout } from './../../redux/actions/actions.js';
+
+export class UserLogged extends connect(store)(LitElement) {
+
+  static get styles() {
+    return css`
+    :host {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      width: fit-content;
+      height: 100%;
+    }
+
+    .user-logo{
+      border-radius: 50%;
+      padding: 5px;
+      background-color: rgb(var(--base-color))
+    }
+
+    .user-logo:hover{
+      border-radius: 50%;
+      padding: 5px;
+      color: rgb(var(--base-color));
+      background-color: rgb(var(--dark-color));
+      cursor: pointer;
+    }
+
+    .options{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      padding: 5px;
+      box-sizing: border-box;
+      right: 15px;
+      top: var( --header-height);
+      background-color: rgb(var(--base-color),0.5);
+    }
+
+    .option:hover{
+      background-color: rgb(var(--dark-color),0.5);
+      cursor: pointer;
+    }
+
+    .hidden{
+      display: none;
+    }
+
+    `;
+  }
+
+  static get properties() {
+    return {
+      userType: {
+        type: String
+      },
+      showDetails: {
+        type: Boolean
+      }
+    };
+  }
+
+  constructor() {
+    super();
+    this.showDetails = false;
+  }
+
+
+  render() {
+
+    return html`
+      <div class="user-logo" @click=${this.manageInfoVisibility}>
+        <p>${this.userType}</p>
+      </div>
+      <div class="options hidden" @mouseleave=${this.hideInfo} >
+        <div class="option" @click=${this.editInfo}>Editar perfil</div>
+        <div class="option" @click=${this.logout}><u>Logout</u></div>
+      </div>
+    `;
+  }
+
+  manageInfoVisibility(){
+    this.showDetails = !this.showDetails;
+    this.showDetails 
+      ? this.shadowRoot.querySelector('.options').classList.remove('hidden') 
+      : this.shadowRoot.querySelector('.options').classList.add('hidden');
+  }
+
+  editInfo(){
+    this.manageInfoVisibility();
+    Router.go('/profile')
+  }
+
+  logout(){
+    this.manageInfoVisibility();
+    store.dispatch(setLogout());
+    
+  }
+
+  hideInfo(){
+    this.showDetails = false;
+    this.shadowRoot.querySelector('.options').classList.add('hidden');
+  }
+
+}
+
+customElements.define('user-logged', UserLogged);
