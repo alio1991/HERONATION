898c4875755ba86c825e17efdd08ec66d5e1b4c2
alio1991 <alioli_15@hotmail.com>
2020-04-27 16:40:09 +0200

    feature: history donations shown in management view

--- src/views/management-view/management-view.js
+++ src/views/management-view/management-view.js
@@ -1,4 +1,5 @@
 import { html, css, LitElement } from 'lit-element';
+import '../../components/donations-history/donations-history.js';
 import { Router } from '@vaadin/router';
 
 export class ManagementView extends LitElement {
@@ -6,10 +7,14 @@ export class ManagementView extends LitElement {
   static get styles() {
     return css`
     :host {
-      display: flex;
-      padding-top: var(--header-height);
       width: 100%;
       height:100%;
+    }
+
+    .management{
+      padding-top: var(--header-height);
+      display: flex;
+      justify-content: space-evenly;
     }
 
     `;
@@ -17,17 +22,33 @@ export class ManagementView extends LitElement {
 
   static get properties() {
     return {
+      donationsHistory: Array
     };
   }
 
   constructor() {
     super();
+    this.donationsHistory = [
+      {
+        nombre: 'DONACION 1',
+        fecha: '3/7/2010',
+        empresa: 'RM ORG'
+      },
+      {
+        nombre: 'DONACION 2',
+        fecha: '30/3/2019',
+        empresa: 'Georfy'
+      }
+    ];
   }
 
 
   render() {
     return html`
-    <h1>MANAGEMENT</h1>
+    <div class="management">
+      <h1>MANAGEMENT</h1>
+      <donations-history .donationsHistory=${this.donationsHistory}></donations-history>
+    </div>
     `;
   }
 
